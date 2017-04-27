5b1f4594a2318b1f092acd1ed8376d760377c3d8
Clément <troniq89@tutomatik.fr>
2017-04-27 19:27:20 +0200

    Ajout librairie jQuery+Infos forum

---
+++ inc/infos.php
@@ -0,0 +1,14 @@
+<?php
+
+
+	$boardinfos = array(
+
+		"name" => "LaFibre.info",
+		"desc" => "Tout sur la fibre optique : FTTH Gpon, P2P, FTTLA, FTTdp,...",
+		"url" => "https://lafibre.info/"
+		"admincontact" => "Vivien GUEANT <redaction@lafibre.info>",
+		"keywords" => "Fibre optique,FTTH,très haut débit,Gpon,FTTLA,FTTdp,Test débit,SpeedTest"
+
+
+	);
+?>

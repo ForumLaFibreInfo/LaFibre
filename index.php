505e59d41f205df4e6d5a6ad7b994d8b8fd89779
Clément <troniq89@openmailbox.org>
2017-04-27 19:54:18 +0200

    Ajout des libs de base

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
     <title>
     <?php echo($boardinfo['nom']); ?>
     </title>
+    <script src="inc/assets/jquery.min.js"></script>
+    <script src="inc/assets/modernizr_custom.min.js"></script>
+    <script src="inc/assets/prefixfree.min.js"></script>
+    <script src="inc/assets/prefixfree.jquery.js"></script>
+
   </head>
   <body>
   <!-- Hello world -->

d88e67184f1ed245f20120b3a69de949666cf30e
TroniQ89 <TroniQ89@users.noreply.github.com>
2017-04-27 19:00:26 +0200

    La base
    Faut bien commencer par quelque chose non ?

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+  require('inc/acc_db.php');
+  require('inc/infos.php');
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+    <?php echo($boardinfo['nom']);
+    </title>
+  </head>
+  <body>
+  <!-- Hello world -->
+  </body>
+</html>
